04e1ddff2cedd845e24448620f29da3cb342c913
Louis <instantwazzer@gmx.de>
2020-03-20 14:29:04 +0100

    all working fine

---
+++ AuswertungsSoftware/FormAuswertung.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AuswertungsSoftware
+{
+    public partial class FrmAuswertung : Form
+    {
+        int scoreTeamA = 0;
+        int scoreTeamB = 0;
+        int scoreTeamC = 0;
+        int scoreTeamD = 0;
+
+        int previousScoreTeamA = 0;
+        int previousScoreTeamB = 0;
+        int previousScoreTeamC = 0;
+        int previousScoreTeamD = 0;
+
+
+        /* Methods */
+
+        private void printScores()
+        {
+            lblTeamAScore.Text = "Team A: " + scoreTeamA;
+            lblTeamBScore.Text = "Team B: " + scoreTeamB;
+            lblTeamCScore.Text = "Team C: " + scoreTeamC;
+            lblTeamDScore.Text = "Team D: " + scoreTeamD;
+        }
+
+
+        public FrmAuswertung()
+        {
+            InitializeComponent();
+            printScores();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void bntFullscreen_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Maximized;
+        }
+
+        private void btnTeamAPlus_Click(object sender, EventArgs e)
+        {
+            scoreTeamA++;
+
+            printScores();
+        }
+
+        private void btnTeamAMinus_Click(object sender, EventArgs e)
+        {
+            scoreTeamA--;
+
+            if (scoreTeamA < 0)
+            {
+                scoreTeamA = 0;
+            }
+
+            printScores();
+        }
+
+
+        private void btnTeamBPlus_Click(object sender, EventArgs e)
+        {
+            scoreTeamB++;
+
+            printScores();
+        }
+
+        private void btnTeamBMinus_Click(object sender, EventArgs e)
+        {
+            scoreTeamB--;
+
+            if (scoreTeamB < 0)
+            {
+                scoreTeamB = 0;
+            }
+
+            printScores();
+        }
+
+
+        private void btnTeamCPlus_Click(object sender, EventArgs e)
+        {
+            scoreTeamC++;
+
+            printScores();
+        }
+
+        private void btnTeamCMinus_Click(object sender, EventArgs e)
+        {
+            scoreTeamC--;
+
+            if (scoreTeamC < 0)
+            {
+                scoreTeamC = 0;
+            }
+
+            printScores();
+        }
+
+
+        private void btnTeamDPlus_Click(object sender, EventArgs e)
+        {
+            scoreTeamD++;
+
+            printScores();
+        }
+
+        private void btnTeamDMinus_Click(object sender, EventArgs e)
+        {
+            scoreTeamD--;
+
+            if (scoreTeamD < 0)
+            {
+                scoreTeamD = 0;
+            }
+
+            printScores();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            /* Saving the Values */
+            previousScoreTeamA = scoreTeamA;
+            previousScoreTeamB = scoreTeamB;
+            previousScoreTeamC = scoreTeamC;
+            previousScoreTeamD = scoreTeamD;
+
+            /* Actually reseting them */
+            scoreTeamA = 0;
+            scoreTeamB = 0;
+            scoreTeamC = 0;
+            scoreTeamD = 0;
+
+            printScores();
+        }
+
+        private void btnRestorePrevious_Click(object sender, EventArgs e)
+        {
+            scoreTeamA = previousScoreTeamA;
+            scoreTeamB = previousScoreTeamB;
+            scoreTeamC = previousScoreTeamC;
+            scoreTeamD = previousScoreTeamD;
+
+            printScores();
+        }
+    }
+}
---
+++ FormAuswertung.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace AuswertungsSoftware
+{
+    partial class FrmAuswertung
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTeamRindFleichEtikettierungsAufgabenUeberwachungsÜbertragungsSchutzgesetz = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnTeamAPlus = new System.Windows.Forms.Button();
+            this.btnTeamBPlus = new System.Windows.Forms.Button();
+            this.btnTeamCPlus = new System.Windows.Forms.Button();
+            this.btnTeamDPlus = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.bntFullscreen = new System.Windows.Forms.Button();
+            this.btnTeamAMinus = new System.Windows.Forms.Button();
+            this.btnTeamBMinus = new System.Windows.Forms.Button();
+            this.btnTeamCMinus = new System.Windows.Forms.Button();
+            this.btnTeamDMinus = new System.Windows.Forms.Button();
+            this.lblTeamAScore = new System.Windows.Forms.Label();
+            this.lblTeamBScore = new System.Windows.Forms.Label();
+            this.lblTeamCScore = new System.Windows.Forms.Label();
+            this.lblTeamDScore = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnRestorePrevious = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTeamRindFleichEtikettierungsAufgabenUeberwachungsÜbertragungsSchutzgesetz
+            // 
+            this.lblTeamRindFleichEtikettierungsAufgabenUeberwachungsÜbertragungsSchutzgesetz.AutoSize = true;
+            this.lblTeamRindFleichEtikettierungsAufgabenUeberwachungsÜbertragungsSchutzgesetz.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTeamRindFleichEtikettierungsAufgabenUeberwachungsÜbertragungsSchutzgesetz.Location = new System.Drawing.Point(50, 100);
+            this.lblTeamRindFleichEtikettierungsAufgabenUeberwachungsÜbertragungsSchutzgesetz.Name = "lblTeamRindFleichEtikettierungsAufgabenUeberwachungsÜbertragungsSchutzgesetz";
+            this.lblTeamRindFleichEtikettierungsAufgabenUeberwachungsÜbertragungsSchutzgesetz.Size = new System.Drawing.Size(61, 20);
+            this.lblTeamRindFleichEtikettierungsAufgabenUeberwachungsÜbertragungsSchutzgesetz.TabIndex = 0;
+            this.lblTeamRindFleichEtikettierungsAufgabenUeberwachungsÜbertragungsSchutzgesetz.Text = "Team A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(256, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Team B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(462, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Team C";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(668, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Team D";
+            // 
+            // btnTeamAPlus
+            // 
+            this.btnTeamAPlus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTeamAPlus.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTeamAPlus.Location = new System.Drawing.Point(21, 123);
+            this.btnTeamAPlus.Name = "btnTeamAPlus";
+            this.btnTeamAPlus.Size = new System.Drawing.Size(111, 44);
+            this.btnTeamAPlus.TabIndex = 1;
+            this.btnTeamAPlus.Text = "+1";
+            this.btnTeamAPlus.UseVisualStyleBackColor = true;
+            this.btnTeamAPlus.Click += new System.EventHandler(this.btnTeamAPlus_Click);
+            // 
+            // btnTeamBPlus
+            // 
+            this.btnTeamBPlus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTeamBPlus.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTeamBPlus.Location = new System.Drawing.Point(228, 123);
+            this.btnTeamBPlus.Name = "btnTeamBPlus";
+            this.btnTeamBPlus.Size = new System.Drawing.Size(111, 44);
+            this.btnTeamBPlus.TabIndex = 3;
+            this.btnTeamBPlus.Text = "+1";
+            this.btnTeamBPlus.UseVisualStyleBackColor = true;
+            this.btnTeamBPlus.Click += new System.EventHandler(this.btnTeamBPlus_Click);
+            // 
+            // btnTeamCPlus
+            // 
+            this.btnTeamCPlus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTeamCPlus.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTeamCPlus.Location = new System.Drawing.Point(435, 123);
+            this.btnTeamCPlus.Name = "btnTeamCPlus";
+            this.btnTeamCPlus.Size = new System.Drawing.Size(111, 44);
+            this.btnTeamCPlus.TabIndex = 5;
+            this.btnTeamCPlus.Text = "+1";
+            this.btnTeamCPlus.UseVisualStyleBackColor = true;
+            this.btnTeamCPlus.Click += new System.EventHandler(this.btnTeamCPlus_Click);
+            // 
+            // btnTeamDPlus
+            // 
+            this.btnTeamDPlus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTeamDPlus.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTeamDPlus.Location = new System.Drawing.Point(642, 123);
+            this.btnTeamDPlus.Name = "btnTeamDPlus";
+            this.btnTeamDPlus.Size = new System.Drawing.Size(111, 44);
+            this.btnTeamDPlus.TabIndex = 7;
+            this.btnTeamDPlus.Text = "+1";
+            this.btnTeamDPlus.UseVisualStyleBackColor = true;
+            this.btnTeamDPlus.Click += new System.EventHandler(this.btnTeamDPlus_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(686, 407);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(82, 31);
+            this.btnClose.TabIndex = 12;
+            this.btnClose.Text = "Kloß";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // bntFullscreen
+            // 
+            this.bntFullscreen.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntFullscreen.Location = new System.Drawing.Point(12, 407);
+            this.bntFullscreen.Name = "bntFullscreen";
+            this.bntFullscreen.Size = new System.Drawing.Size(90, 31);
+            this.bntFullscreen.TabIndex = 9;
+            this.bntFullscreen.Text = "Fullscreen";
+            this.bntFullscreen.UseVisualStyleBackColor = true;
+            this.bntFullscreen.Click += new System.EventHandler(this.bntFullscreen_Click);
+            // 
+            // btnTeamAMinus
+            // 
+            this.btnTeamAMinus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTeamAMinus.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTeamAMinus.Location = new System.Drawing.Point(21, 173);
+            this.btnTeamAMinus.Name = "btnTeamAMinus";
+            this.btnTeamAMinus.Size = new System.Drawing.Size(111, 44);
+            this.btnTeamAMinus.TabIndex = 2;
+            this.btnTeamAMinus.Text = "-1";
+            this.btnTeamAMinus.UseVisualStyleBackColor = true;
+            this.btnTeamAMinus.Click += new System.EventHandler(this.btnTeamAMinus_Click);
+            // 
+            // btnTeamBMinus
+            // 
+            this.btnTeamBMinus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTeamBMinus.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTeamBMinus.Location = new System.Drawing.Point(228, 173);
+            this.btnTeamBMinus.Name = "btnTeamBMinus";
+            this.btnTeamBMinus.Size = new System.Drawing.Size(111, 44);
+            this.btnTeamBMinus.TabIndex = 4;
+            this.btnTeamBMinus.Text = "-1";
+            this.btnTeamBMinus.UseVisualStyleBackColor = true;
+            this.btnTeamBMinus.Click += new System.EventHandler(this.btnTeamBMinus_Click);
+            // 
+            // btnTeamCMinus
+            // 
+            this.btnTeamCMinus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTeamCMinus.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTeamCMinus.Location = new System.Drawing.Point(435, 173);
+            this.btnTeamCMinus.Name = "btnTeamCMinus";
+            this.btnTeamCMinus.Size = new System.Drawing.Size(111, 44);
+            this.btnTeamCMinus.TabIndex = 6;
+            this.btnTeamCMinus.Text = "-1";
+            this.btnTeamCMinus.UseVisualStyleBackColor = true;
+            this.btnTeamCMinus.Click += new System.EventHandler(this.btnTeamCMinus_Click);
+            // 
+            // btnTeamDMinus
+            // 
+            this.btnTeamDMinus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTeamDMinus.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTeamDMinus.Location = new System.Drawing.Point(642, 173);
+            this.btnTeamDMinus.Name = "btnTeamDMinus";
+            this.btnTeamDMinus.Size = new System.Drawing.Size(111, 44);
+            this.btnTeamDMinus.TabIndex = 8;
+            this.btnTeamDMinus.Text = "-1";
+            this.btnTeamDMinus.UseVisualStyleBackColor = true;
+            this.btnTeamDMinus.Click += new System.EventHandler(this.btnTeamDMinus_Click);
+            // 
+            // lblTeamAScore
+            // 
+            this.lblTeamAScore.AutoSize = true;
+            this.lblTeamAScore.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTeamAScore.Location = new System.Drawing.Point(28, 262);
+            this.lblTeamAScore.Name = "lblTeamAScore";
+            this.lblTeamAScore.Size = new System.Drawing.Size(61, 20);
+            this.lblTeamAScore.TabIndex = 11;
+            this.lblTeamAScore.Text = "Team A";
+            // 
+            // lblTeamBScore
+            // 
+            this.lblTeamBScore.AutoSize = true;
+            this.lblTeamBScore.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTeamBScore.Location = new System.Drawing.Point(235, 262);
+            this.lblTeamBScore.Name = "lblTeamBScore";
+            this.lblTeamBScore.Size = new System.Drawing.Size(59, 20);
+            this.lblTeamBScore.TabIndex = 12;
+            this.lblTeamBScore.Text = "Team B";
+            // 
+            // lblTeamCScore
+            // 
+            this.lblTeamCScore.AutoSize = true;
+            this.lblTeamCScore.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTeamCScore.Location = new System.Drawing.Point(442, 262);
+            this.lblTeamCScore.Name = "lblTeamCScore";
+            this.lblTeamCScore.Size = new System.Drawing.Size(59, 20);
+            this.lblTeamCScore.TabIndex = 13;
+            this.lblTeamCScore.Text = "Team C";
+            // 
+            // lblTeamDScore
+            // 
+            this.lblTeamDScore.AutoSize = true;
+            this.lblTeamDScore.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTeamDScore.Location = new System.Drawing.Point(649, 262);
+            this.lblTeamDScore.Name = "lblTeamDScore";
+            this.lblTeamDScore.Size = new System.Drawing.Size(61, 20);
+            this.lblTeamDScore.TabIndex = 14;
+            this.lblTeamDScore.Text = "Team D";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(228, 370);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(154, 31);
+            this.btnReset.TabIndex = 10;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnRestorePrevious
+            // 
+            this.btnRestorePrevious.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestorePrevious.Location = new System.Drawing.Point(392, 370);
+            this.btnRestorePrevious.Name = "btnRestorePrevious";
+            this.btnRestorePrevious.Size = new System.Drawing.Size(154, 31);
+            this.btnRestorePrevious.TabIndex = 11;
+            this.btnRestorePrevious.Text = "Restore previous";
+            this.btnRestorePrevious.UseVisualStyleBackColor = true;
+            this.btnRestorePrevious.Click += new System.EventHandler(this.btnRestorePrevious_Click);
+            // 
+            // FrmAuswertung
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 450);
+            this.Controls.Add(this.btnRestorePrevious);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblTeamDScore);
+            this.Controls.Add(this.lblTeamCScore);
+            this.Controls.Add(this.lblTeamBScore);
+            this.Controls.Add(this.lblTeamAScore);
+            this.Controls.Add(this.btnTeamDMinus);
+            this.Controls.Add(this.btnTeamCMinus);
+            this.Controls.Add(this.btnTeamBMinus);
+            this.Controls.Add(this.btnTeamAMinus);
+            this.Controls.Add(this.bntFullscreen);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnTeamDPlus);
+            this.Controls.Add(this.btnTeamCPlus);
+            this.Controls.Add(this.btnTeamBPlus);
+            this.Controls.Add(this.btnTeamAPlus);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblTeamRindFleichEtikettierungsAufgabenUeberwachungsÜbertragungsSchutzgesetz);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FrmAuswertung";
+            this.Text = "Auswertung";
+            this.Load += new System.EventHandler(this.FrmAuswertung_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTeamRindFleichEtikettierungsAufgabenUeberwachungsÜbertragungsSchutzgesetz;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnTeamAPlus;
+        private System.Windows.Forms.Button btnTeamBPlus;
+        private System.Windows.Forms.Button btnTeamCPlus;
+        private System.Windows.Forms.Button btnTeamDPlus;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button bntFullscreen;
+        private System.Windows.Forms.Button btnTeamAMinus;
+        private System.Windows.Forms.Button btnTeamBMinus;
+        private System.Windows.Forms.Button btnTeamCMinus;
+        private System.Windows.Forms.Button btnTeamDMinus;
+        private System.Windows.Forms.Label lblTeamAScore;
+        private System.Windows.Forms.Label lblTeamBScore;
+        private System.Windows.Forms.Label lblTeamCScore;
+        private System.Windows.Forms.Label lblTeamDScore;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnRestorePrevious;
+    }
+}
+
